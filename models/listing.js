291cfc6f0467365cb12fe4b6bdd041f3f66268cb
sumit mule <sumitmule.dev.work@gmail.com>
2025-10-01 15:20:38 +0530

    added reviews.js

--- models/listing.js
+++ models/listing.js
@@ -14,6 +14,12 @@
   price: Number,
   location: String,
   country: String,
+  reviews:[
+    {
+      type: Schema.Types.ObjectId,
+      ref:"Reviews",
+    },
+  ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
